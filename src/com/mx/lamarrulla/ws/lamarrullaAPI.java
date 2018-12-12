dcc5d191b32c6a116fc03a89980ae8bdfbcdadfe
jose david rincon angeles <angelnmara@hotmail.com>
2018-12-11 18:49:25 -0600

    se agrega delete y post

--- src/com/mx/lamarrulla/ws/lamarrullaAPI.java
+++ src/com/mx/lamarrulla/ws/lamarrullaAPI.java
@@ -1,5 +1,10 @@
 package com.mx.lamarrulla.ws;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,6 +15,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,8 +25,8 @@ import com.mx.lamarrulla.implement.implementAPI;
 
 @Path("lamarrullaAPI")
 @Secured
-@Consumes("application/json")
-@Produces("application/json")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class lamarrullaAPI {
 	
 	implementAPI objAPI = new implementAPI();
@@ -29,13 +35,8 @@ public class lamarrullaAPI {
 	public lamarrullaAPI() {}
 
 	@GET
-	@Path("{tabla}")
-	public String apiSelect(@PathParam("tabla") String tabla, @PathParam("idTabla") String idTabla) throws JSONException {
-		// 
-		//@Context HttpHeaders headers
-		//headers.getHeaderString("tabla")
-		//headers.getHeaderString("tabla")
-		//jso = new JSONObject("{" + tabla + " : " + objAPI.getstJS() + "}");
+	@Path("{tabla}/{idTabla}")
+	public String apiSelect(@PathParam("tabla") String tabla, @PathParam("idTabla") String idTabla) throws JSONException {		
 		try {			
 			System.out.println(tabla + " " + idTabla);
 			if(idTabla==null) {
@@ -51,32 +52,88 @@ public class lamarrullaAPI {
 		}						
 		return jso.toString();
 	}	
-//	@DELETE
-//	public String apiDelete(@PathParam("tabla") String tabla, @PathParam("idTabla") int idTabla) throws JSONException {
+	@DELETE
+	@Path("{tabla}/{idTabla}")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String apiDelete(@PathParam("tabla") String tabla, @PathParam("idTabla") String idTabla) throws JSONException {
+		try {			
+			if(idTabla==null || idTabla == "0") {
+				System.out.println("no se procesara idtabla = " + idTabla);
+				jso = new JSONObject("{error:\"Se tiene que seleccionar el identificador a borrar\"}");				
+			}else {
+				objAPI.setConsulta("select * from fnAPI(4, '"+ tabla + "', '', '', " + idTabla + ");");
+				objAPI.ejecutaAPI();
+				System.out.println(objAPI.getstJS());
+				jso = new JSONObject(objAPI.getstJS());
+			}		
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			jso = new JSONObject("{error:\"" + ex.getMessage() + "\"}");
+		}						
+		return jso.toString();
+	}	
+	@POST
+	@Path("{tabla}")
+	public String apiInsert(@PathParam("tabla") String tabla, InputStream incomingData) throws JSONException {
+		//@Context HttpHeaders headers
 //		try {			
-//			objAPI.setConsulta("select * from fnAPI(4, '"+ tabla + "', '', '', " + idTabla + ");");
-//			objAPI.ejecutaAPI();			
-//			System.out.println(objAPI.getstJS());
-//		}catch(Exception ex) {
-//			System.out.println(ex.getMessage());
-//			jso = new JSONObject("{error:\"" + ex.getMessage() + "\"}");
-//		}						
-//		return jso.toString();
-//	}	
-//	@POST
-//	public String apiInsert(@PathParam("tabla") String tabla, @Context HttpHeaders headers) throws JSONException {				
-//		try {			
-//			String campos = headers.getHeaderString("campos");
-//			String valores = headers.getHeaderString("valores");
+//			String campos = ""; //= headers.getHeaderString("campos");
+//			String valores = ""; //= headers.getHeaderString("valores");
+//			System.out.println(jso.toString());
 //			objAPI.setConsulta("select * from fnAPI(2, '"+ tabla + "', " + campos + ", " + valores + ", 0);");
-//			objAPI.ejecutaAPI();			
+//			objAPI.ejecutaAPI();
 //			System.out.println(objAPI.getstJS());
 //		}catch(Exception ex) {
 //			System.out.println(ex.getMessage());
 //			jso = new JSONObject("{error:\"" + ex.getMessage() + "\"}");
-//		}						
-//		return jso.toString();
-//	}
+//		}
+		StringBuilder crunchifyBuilder = new StringBuilder();		
+		String campos = "";
+		String valores = "";
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                crunchifyBuilder.append(line);
+            }
+            jso = new JSONObject(crunchifyBuilder.toString());
+            @SuppressWarnings("unchecked")
+			Iterator<String> keys = jso.keys();
+            String[] CamposA = new String[jso.length()];
+            String[] ValoresA = new String[jso.length()];
+            String ValorPaso = "";
+            int i = 0;
+            
+            while(keys.hasNext()) {
+                String key = keys.next();                
+                if (jso.get(key) instanceof Integer) {
+                	System.out.println(jso.getInt(key));
+                	ValorPaso = String.valueOf(jso.getInt(key));
+                }
+                else if(jso.get(key) instanceof Boolean) {
+                	System.out.println(jso.getBoolean(key));
+                	ValorPaso = String.valueOf(jso.getBoolean(key));
+                }else {
+                	System.out.println(jso.getString(key));
+                	ValorPaso = "''" + jso.getString(key) + "''";
+                }
+                CamposA[i] = key;
+                ValoresA[i] = ValorPaso;
+                i++;
+            }
+            campos = "(" + String.join(",", CamposA) + ")";
+            valores = "(" + String.join(",", ValoresA) + ")";
+            objAPI.setConsulta("select * from fnAPI(2, '"+ tabla + "', '" + campos + "', '" + valores + "', 0);");
+            objAPI.ejecutaAPI();
+            jso = new JSONObject(objAPI.getstJS());
+        } catch (Exception e) {
+            System.out.println("Error Parsing: - ");
+            jso = new JSONObject("{error:\"Ocurrio un error al extraer el body\"}") ;
+        }
+        //System.out.println("Data Received: " + crunchifyBuilder.toString());
+        //JSONObject jso = new JSONObject(crunchifyBuilder.toString());        
+		return jso.toString();
+	}
 //	@PUT	
 //	public String apiUpdate(@PathParam("tabla") String tabla, @PathParam("id") int idTabla, @Context HttpHeaders headers) throws JSONException {				
 //		try {			
