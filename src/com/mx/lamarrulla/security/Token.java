6fc33027ec2149e4f6e48c8bfe8e199d53abc92c
Dave R <angelnmara@hotmail.com>
2019-03-28 01:23:48 -0600

    se agrega fecha de expiracion al token, la validación no se esta haciendo correctamente se tiene que arreglar

--- src/com/mx/lamarrulla/security/Token.java
+++ src/com/mx/lamarrulla/security/Token.java
@@ -1,11 +1,15 @@
 package com.mx.lamarrulla.security;
 
+import java.util.Date;
+
 //import java.security.Key;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.JWTVerifier.BaseVerification;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.Clock;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class Token {
@@ -14,6 +18,7 @@ public class Token {
 	private String secret;
 	private String token;
 	private boolean autenticado = false;
+	private Clock clock;
 		
 	public boolean isAutenticado() {
 		return autenticado;
@@ -49,9 +54,14 @@ public class Token {
 
 	public void CreateToken() {		
 		try {
+			Date date = new Date();			
+			System.out.print(date);
 		    Algorithm algorithm = Algorithm.HMAC256(secret);
 		    token = JWT.create()
 		        .withIssuer(user)
+		        .withNotBefore(date)
+		        .withExpiresAt(date)
+		        .withIssuedAt(date)		      
 		        .sign(algorithm);
 		} catch (JWTCreationException exception){
 		    //Invalid Signing configuration / Couldn't convert Claims.
@@ -61,9 +71,11 @@ public class Token {
 	public void VerifyToken() {
 		try {
 			Algorithm algorithm = Algorithm.HMAC256(secret);
-		    JWTVerifier verifier = JWT.require(algorithm)
-		        .withIssuer(user)
-		        .build(); //Reusable verifier instance
+		    JWTVerifier verifier = JWT.require(algorithm)		    		
+		    		.acceptExpiresAt(180)		    		
+		    		.acceptNotBefore(0)
+		    		.acceptIssuedAt(0)
+		    		.build();			
 		    DecodedJWT jwt = verifier.verify(token);
 		    autenticado = true;		    
 		}catch(Exception ex) {
